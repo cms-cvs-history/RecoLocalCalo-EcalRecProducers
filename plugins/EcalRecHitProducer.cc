d1b1d62ab7fe999e77fd8eba9368204aed9e0645
cmsbuild <>
2009-12-14 13:57:51 +0000

    This commit was manufactured by cvs2git to create tag 'RecoLocalCalo- EcalRecProducers-V02-00-42-04'.
    Sprout from master 2009-11-09 15:45:36 UTC Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch> 'fix missing bitmask'
    Cherrypick from master 2009-12-14 13:57:50 UTC Chia-Ming Kuo <sha1-1540d18cc0bb68ff0746e8491e1d31e5651fe08e@cern.ch> 'cfi for ES rechit reconstruction with fit method':
        plugins/ESRecHitWorker.cc
        plugins/ESRecHitWorker.h
        plugins/EcalRecHitProducer.cc
        python/ecalPreshowerRecHitFit_cfi.py

--- plugins/EcalRecHitProducer.cc
+++ plugins/EcalRecHitProducer.cc
@@ -1,9 +1,9 @@
 /** \class EcalRecHitProducer
  *   produce ECAL rechits from uncalibrated rechits
  *
- *  $Id: EcalRecHitProducer.cc,v 1.8 2009/10/16 15:48:11 ferriff Exp $
- *  $Date: 2009/10/16 15:48:11 $
- *  $Revision: 1.8 $
+ *  $Id: EcalRecHitProducer.cc,v 1.10 2009/11/15 11:33:51 franzoni Exp $
+ *  $Date: 2009/11/15 11:33:51 $
+ *  $Revision: 1.10 $
  *  \author Shahram Rahatlou, University of Rome & INFN, March 2006
  *
  **/
@@ -246,8 +246,10 @@ EcalRecHitProducer::produce(edm::Event& evt, const edm::EventSetup& es)
                 if ( scIds ) {
                         for( std::set<EcalScDetId>::const_iterator it = scIds->begin(); it != scIds->end(); ++it ) {
                                 // uses the EcalUncalibratedRecHit to pass the DetId info
-                                EcalUncalibratedRecHit urh( EEDetId( ((*it).ix()-1)*5+1, ((*it).iy()-1)*5+1, (*it).zside() ), 0, 0, 0, 0, EcalRecHitWorkerBaseClass::EE_FE );
-                                workerRecover_->run( evt, urh, *eeRecHits );
+                                if (EEDetId::validDetId( ((*it).ix()-1)*5+1, ((*it).iy()-1)*5+1, (*it).zside() )) {
+                                        EcalUncalibratedRecHit urh( EEDetId( ((*it).ix()-1)*5+1, ((*it).iy()-1)*5+1, (*it).zside() ), 0, 0, 0, 0, EcalRecHitWorkerBaseClass::EE_FE );
+                                        workerRecover_->run( evt, urh, *eeRecHits );
+                                }
                         }
                 }
         }
