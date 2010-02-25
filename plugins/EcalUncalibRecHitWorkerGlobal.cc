59e32760c57940d149d7c814b1abfe5947524d5b
cmsbuild <>
2010-02-25 18:12:01 +0000

    This commit was manufactured by cvs2git to create tag 'RecoLocalCalo- EcalRecProducers-abeBis100225-onTopOf-ff100208-04'.
    Sprout from master 2010-02-08 14:25:56 UTC Federico Ferri <sha1-fe68d1312e31c44a0fe711df9628a02eb52f6e9f@cern.ch> 'add kFake flag for spike signals'
    Cherrypick from master 2009-10-25 23:30:22 UTC Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch> 'upgrade of ratio to V3 and out-of-time energy/amplitude using 13 bits (from S. Cooper / S. Ledovskoy)':
        plugins/EcalDetIdToBeRecoveredProducer.cc
        plugins/EcalDetIdToBeRecoveredProducer.h
        plugins/EcalRecHitWorkerRecover.cc
        plugins/EcalUncalibRecHitWorkerRatio.cc
    Cherrypick from master 2010-02-25 18:12:00 UTC Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch> 'fix from Abe: check has to be performed on time calibration map, not amplitude map':
        plugins/EcalRecHitWorkerSimple.cc
        plugins/EcalUncalibRecHitWorkerGlobal.cc

--- plugins/EcalUncalibRecHitWorkerGlobal.cc
+++ plugins/EcalUncalibRecHitWorkerGlobal.cc
@@ -191,6 +191,7 @@ EcalUncalibRecHitWorkerGlobal::run( const edm::Event & evt,
 
                 // === time computation ===
                 // ratio method
+                float clockToNsConstant = 25.;
                 if (detid.subdetId()==EcalEndcap) {
                                 ratioMethod_endcap_.init( *itdg, pedVec, pedRMSVec, gainRatios );
                                 ratioMethod_endcap_.computeTime( EEtimeFitParameters_, EEtimeFitLimits_, EEamplitudeFitParameters_ );
@@ -199,7 +200,7 @@ EcalUncalibRecHitWorkerGlobal::run( const edm::Event & evt,
                                 uncalibRecHit.setJitter( crh.timeMax - 5 );
                                 uncalibRecHit.setOutOfTimeEnergy( crh.amplitudeMax );
                                 if ( uncalibRecHit.amplitude() > amplitudeThreshEE_
-                                     && fabs(crh.timeMax-5) > outOfTimeThresh_ ) {
+                                     && fabs( ((crh.timeMax-5) * clockToNsConstant + itimeconst) / clockToNsConstant ) > outOfTimeThresh_ ) {
                                         uncalibRecHit.setRecoFlag( EcalUncalibratedRecHit::kOutOfTime );
                                 }
                 } else {
@@ -210,7 +211,7 @@ EcalUncalibRecHitWorkerGlobal::run( const edm::Event & evt,
                                 uncalibRecHit.setJitter( crh.timeMax - 5 );
                                 uncalibRecHit.setOutOfTimeEnergy( crh.amplitudeMax );
                                 if ( uncalibRecHit.amplitude() > amplitudeThreshEB_ && 
-                                     fabs(crh.timeMax-5) > outOfTimeThresh_ ) {
+                                     fabs( ((crh.timeMax-5) * clockToNsConstant + itimeconst) / clockToNsConstant) > outOfTimeThresh_ ) {
                                         uncalibRecHit.setRecoFlag( EcalUncalibratedRecHit::kOutOfTime );
                                 }
                 }
@@ -260,10 +261,10 @@ EcalUncalibRecHitWorkerGlobal::run( const edm::Event & evt,
 		    uncalibRecHit.setOutOfTimeChi2(chi2OutOfTime);
 		}
         }
-        if ( detid.subdetId()==EcalEndcap ) {
-                if ( uncalibRecHit.time() > -5 ) {
+        if ( detid.subdetId()==EcalBarrel ) {
+                if ( uncalibRecHit.jitter()*25. > -5 ) {
                         EBDataFrame dt(*itdg);
-                        if ( dt.spikeEstimator() > ebSpikeThresh_ ) uncalibRecHit.setRecoFlag( EcalUncalibratedRecHit::kFake );
+                        if ( dt.spikeEstimator() < ebSpikeThresh_ ) uncalibRecHit.setRecoFlag( EcalUncalibratedRecHit::kFake );
                 }
         }
 
